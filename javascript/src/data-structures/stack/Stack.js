64518d5584b85a2e66e25cbd19e96b7e38cd984a
Huang Jian <1342042894@qq.com>
2019-10-11 14:07:23 +0800

    add stack test

--- javascript/src/data-structures/stack/Stack.js
+++ javascript/src/data-structures/stack/Stack.js
@@ -1,14 +1,18 @@
 const { List, Element: ListElement } = require("../list/List.js");
 
+// Stack first in last out.
 class Stack {
   constructor() {
     this.list = new List();
   }
 
+  // isEmpty returns whether stack is empty.
+  // @return [boolean]
   isEmpty() {
     return this.list.Len() === 0;
   }
 
+  // peek returns the stack top element.
   peek() {
     const e = this.list.Front();
     if (e === null) {
@@ -17,10 +21,12 @@ class Stack {
     return e.value;
   }
 
+  // push pushes value to stack top;
   push(value) {
     this.list.PushFront(value);
   }
 
+  // pop remove the top element from stack, and returns it.
   pop() {
     const e = this.list.Front();
     if (e === null) {
@@ -28,6 +34,14 @@ class Stack {
     }
     this.list.Remove(e);
     return e.value;
+  }
+
+  toArray() {
+    return this.list.toArray().map(elem => elem.value);
+  }
+
+  toString(callback) {
+    return this.list.toString(callback);
   }
 }
 
