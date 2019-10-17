37f7eb7fe42aec06490f22beba4de11313cb7d3c
huangjian <1342042894@qq.com>
2019-10-17 14:15:50 +0800

    add disjoint set unit test

--- javascript/src/data-structures/disjoint-set/__test__/DisjointSetItem.test.js
+++ javascript/src/data-structures/disjoint-set/__test__/DisjointSetItem.test.js
@@ -62,10 +62,56 @@ describe("DisjointSetItem", () => {
     };
 
     const itemA = new DisjointSetItem({ key: "A", value: 1 }, keyExtractor);
-    // const itemB = new DisjointSetItem({ key: "A", value: 1 }, keyExtractor);
-    // const itemC = new DisjointSetItem({ key: "A", value: 1 }, keyExtractor);
-    // const itemD = new DisjointSetItem({ key: "A", value: 1 }, keyExtractor);
+    const itemB = new DisjointSetItem({ key: "B", value: 1 }, keyExtractor);
+    const itemC = new DisjointSetItem({ key: "C", value: 1 }, keyExtractor);
+    const itemD = new DisjointSetItem({ key: "D", value: 1 }, keyExtractor);
 
     expect(itemA.getRank()).toBe(0);
+    expect(itemA.getChildren()).toEqual([]);
+    expect(itemA.getKey()).toBe("A");
+    expect(itemA.getRoot()).toEqual(itemA);
+    expect(itemA.isRoot()).toBe(true);
+    expect(itemB.isRoot()).toBe(true);
+
+    itemA.addChild(itemB);
+    itemD.setParent(itemC);
+
+    expect(itemA.getRank()).toBe(1);
+    expect(itemC.getRank()).toBe(1);
+
+    expect(itemB.getRank()).toBe(0);
+    expect(itemD.getRank()).toBe(0);
+
+    expect(itemA.getChildren().length).toBe(1);
+    expect(itemC.getChildren().length).toBe(1);
+
+    expect(itemA.getChildren()[0]).toEqual(itemB);
+    expect(itemC.getChildren()[0]).toEqual(itemD);
+
+    expect(itemB.getChildren().length).toBe(0);
+    expect(itemD.getChildren().length).toBe(0);
+
+    expect(itemA.getRoot()).toEqual(itemA);
+    expect(itemB.getRoot()).toEqual(itemA);
+
+    expect(itemC.getRoot()).toEqual(itemC);
+    expect(itemD.getRoot()).toEqual(itemC);
+
+    expect(itemA.isRoot()).toBe(true);
+    expect(itemB.isRoot()).toBe(false);
+    expect(itemC.isRoot()).toBe(true);
+    expect(itemD.isRoot()).toBe(false);
+
+    itemA.addChild(itemC);
+
+    expect(itemA.isRoot()).toBe(true);
+    expect(itemB.isRoot()).toBe(false);
+    expect(itemC.isRoot()).toBe(false);
+    expect(itemD.isRoot()).toBe(false);
+
+    expect(itemA.getRank()).toBe(3);
+    expect(itemB.getRank()).toBe(0);
+    expect(itemC.getRank()).toBe(1);
+    expect(itemD.getRank()).toBe(0);
   });
 });
