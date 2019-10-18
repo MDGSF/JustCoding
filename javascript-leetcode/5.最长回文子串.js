42bde902364c5dcce3c86f2cb3429aa1811f5721
huangjian <1342042894@qq.com>
2019-10-18 16:32:43 +0800

    add js leet 5

---
+++ javascript-leetcode/5.最长回文子串.js
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=5 lang=javascript
+ *
+ * [5] 最长回文子串
+ *
+ * https://leetcode-cn.com/problems/longest-palindromic-substring/description/
+ *
+ * algorithms
+ * Medium (27.40%)
+ * Likes:    1373
+ * Dislikes: 0
+ * Total Accepted:    125.8K
+ * Total Submissions: 458.2K
+ * Testcase Example:  '"babad"'
+ *
+ * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+ *
+ * 示例 1：
+ *
+ * 输入: "babad"
+ * 输出: "bab"
+ * 注意: "aba" 也是一个有效答案。
+ *
+ *
+ * 示例 2：
+ *
+ * 输入: "cbbd"
+ * 输出: "bb"
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {};
+
+// @lc code=end
