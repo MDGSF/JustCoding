cea20bc6de5032a16b0c738a066de04ebdc53873
huangjian <1342042894@qq.com>
2019-10-19 17:09:07 +0800

    add js leetcode 5

--- javascript-leetcode/5.最长回文子串.js
+++ javascript-leetcode/5.最长回文子串.js
@@ -35,6 +35,85 @@
  * @param {string} s
  * @return {string}
  */
-var longestPalindrome = function(s) {};
+var longestPalindrome = function(s) {
+  /*
+   https://www.geeksforgeeks.org/manachers-algorithm-linear-time-longest-palindromic-substring-part-1/
+
+  ------------------------------------------------------------
+   centerLeft  curLeft  centerPosition  curRight  centerRight
+  ------------------------------------------------------------
+
+                  "babadada"
+                  |   b   |   a   |   b   |   a   |   d   |   a   |   d   |   a   |
+                  0   1   0   3   0   3   0   1   0   3   0   5   0   3   0   1   0
+
+position Index    0   1   2   3   4   5   6   7   8   9  10  11  12  13  14  15  16
+string Index          0       1       2       3       4       5       6       7
+   */
+  if (s.length < 2) {
+    return s;
+  }
+
+  const N = 2 * s.length + 1;
+  const L = new Array(N);
+  L[0] = 0;
+  L[1] = 1;
+  let centerPosition = 1;
+  let centerRight = 2;
+
+  let maxLPSLength = 0;
+  let maxLPSCenterPosition = 0;
+
+  for (let i = 2; i < N; i += 1) {
+    let needExpand = false;
+    let curRight = i;
+    let curLeft = 2 * centerPosition - curRight;
+
+    let diff = centerRight - curRight;
+    if (diff > 0) {
+      if (L[curLeft] < diff) {
+        L[curRight] = L[curLeft];
+      } else if (L[curLeft] === diff && curRight === N - 1) {
+        L[curRight] = L[curLeft];
+      } else if (L[curLeft] === diff && curRight < N - 1) {
+        L[curRight] = L[curLeft];
+        needExpand = true;
+      } else if (L[curLeft] > diff) {
+        L[curRight] = diff;
+        needExpand = true;
+      }
+    } else {
+      L[curRight] = 0;
+      needExpand = true;
+    }
+
+    if (needExpand) {
+      while (
+        i + L[i] < N &&
+        i - L[i] > 0 &&
+        ((i + L[i] + 1) % 2 == 0 ||
+          (Math.floor((i + L[i] + 1) / 2) < s.length &&
+            s[Math.floor((i + L[i] + 1) / 2)] ==
+              s[Math.floor((i - L[i] - 1) / 2)]))
+      ) {
+        L[i]++;
+      }
+    }
+
+    if (L[curRight] > maxLPSLength) {
+      maxLPSLength = L[curRight];
+      maxLPSCenterPosition = curRight;
+    }
+
+    if (curRight + L[curRight] >= centerRight) {
+      centerPosition = curRight;
+      centerRight = curRight + L[curRight];
+    }
+  }
+
+  const start = Math.floor((maxLPSCenterPosition - maxLPSLength) / 2);
+  const end = start + maxLPSLength;
+  return s.substring(start, end);
+};
 
 // @lc code=end
---
+++ javascript-leetcode/6.z-字形变换.js
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode.cn id=6 lang=javascript
+ *
+ * [6] Z 字形变换
+ *
+ * https://leetcode-cn.com/problems/zigzag-conversion/description/
+ *
+ * algorithms
+ * Medium (45.27%)
+ * Likes:    423
+ * Dislikes: 0
+ * Total Accepted:    65.5K
+ * Total Submissions: 144.7K
+ * Testcase Example:  '"PAYPALISHIRING"\n3'
+ *
+ * 将一个给定字符串根据给定的行数，以从上往下、从左到右进行 Z 字形排列。
+ * 
+ * 比如输入字符串为 "LEETCODEISHIRING" 行数为 3 时，排列如下：
+ * 
+ * L   C   I   R
+ * E T O E S I I G
+ * E   D   H   N
+ * 
+ * 
+ * 之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："LCIRETOESIIGEDHN"。
+ * 
+ * 请你实现这个将字符串进行指定行数变换的函数：
+ * 
+ * string convert(string s, int numRows);
+ * 
+ * 示例 1:
+ * 
+ * 输入: s = "LEETCODEISHIRING", numRows = 3
+ * 输出: "LCIRETOESIIGEDHN"
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: s = "LEETCODEISHIRING", numRows = 4
+ * 输出: "LDREOEIIECIHNTSG"
+ * 解释:
+ * 
+ * L     D     R
+ * E   O E   I I
+ * E C   I H   N
+ * T     S     G
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function(s, numRows) {
+    
+};
+// @lc code=end
+
