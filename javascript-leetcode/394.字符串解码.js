2fa5959a57ac6e14f2d2b1a4e78558e49e4ade1d
huangjian <1342042894@qq.com>
2019-10-25 22:49:57 +0800

    add 394 solution backup

--- javascript-leetcode/394.字符串解码.js
+++ javascript-leetcode/394.字符串解码.js
@@ -41,3 +41,31 @@ var decodeString = function(s) {
 };
 // @lc code=end
 
+/*
+const decodeString = (s) =>{
+    let multiply = [];
+    let tempMult = '';
+    let repeatStr = [];
+    let solution = '';
+
+    for (let char of s) {
+       
+        if(!isNaN(char)) {
+            tempMult = `${tempMult}${char}` // ''
+        } else if (char === '['){
+            multiply.push(tempMult); 
+            tempMult = '';
+            
+            repeatStr.push(solution);
+            solution = '';           
+        } else if (char === ']'){
+
+            solution = repeatStr.pop() + solution.repeat(multiply.pop()) 
+            
+        } else {
+            solution += char; 
+        }
+    }
+    return solution;
+}
+*/
