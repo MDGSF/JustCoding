7838a92c8a5e25eb4fc270f579dc535bce8b0da6
huangjian <1342042894@qq.com>
2019-11-04 08:50:48 +0800

    15

--- javascript-leetcode/15.三数之和.js
+++ javascript-leetcode/15.三数之和.js
@@ -15,18 +15,18 @@
  *
  * 给定一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0
  * ？找出所有满足条件且不重复的三元组。
- * 
+ *
  * 注意：答案中不可以包含重复的三元组。
- * 
+ *
  * 例如, 给定数组 nums = [-1, 0, 1, 2, -1, -4]，
- * 
+ *
  * 满足要求的三元组集合为：
  * [
  * ⁠ [-1, 0, 1],
  * ⁠ [-1, -1, 2]
  * ]
- * 
- * 
+ *
+ *
  */
 
 // @lc code=start
@@ -35,7 +35,31 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    
+  if (nums.length < 3) {
+    return [];
+  }
+  nums.sort((a, b) => a - b);
+  const result = [];
+  for (let i = 0; i < nums.length - 2; i += 1) {
+    if (i >= 1 && nums[i] === nums[i - 1]) {
+      continue;
+    }
+
+    const m = {};
+    for (let j = i + 1; j < nums.length; j += 1) {
+      const target = 0 - nums[i] - nums[j];
+      if (nums[j] in m) {
+        result.push([nums[i], nums[j], target]);
+      } else {
+        m[target] = 1;
+      }
+    }
+  }
+  return result;
 };
 // @lc code=end
 
+// [-1, 0, 1, 2, -1, -4]
+// [ [-1, 0, 1], [-1, -1, 2] ]
+const result = threeSum([-1, 0, 1, 2, -1, -4]);
+console.log(result);
