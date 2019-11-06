9c7813beefab38414bf6b46c4b719f54e5f15b91
huangjian <1342042894@qq.com>
2019-11-06 08:21:44 +0800

    add 189

--- javascript-leetcode/189.旋转数组.js
+++ javascript-leetcode/189.旋转数组.js
@@ -14,32 +14,32 @@
  * Testcase Example:  '[1,2,3,4,5,6,7]\n3'
  *
  * 给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
- * 
+ *
  * 示例 1:
- * 
+ *
  * 输入: [1,2,3,4,5,6,7] 和 k = 3
  * 输出: [5,6,7,1,2,3,4]
  * 解释:
  * 向右旋转 1 步: [7,1,2,3,4,5,6]
  * 向右旋转 2 步: [6,7,1,2,3,4,5]
  * 向右旋转 3 步: [5,6,7,1,2,3,4]
- * 
- * 
+ *
+ *
  * 示例 2:
- * 
+ *
  * 输入: [-1,-100,3,99] 和 k = 2
  * 输出: [3,99,-1,-100]
- * 解释: 
+ * 解释:
  * 向右旋转 1 步: [99,-1,-100,3]
  * 向右旋转 2 步: [3,99,-1,-100]
- * 
+ *
  * 说明:
- * 
- * 
+ *
+ *
  * 尽可能想出更多的解决方案，至少有三种不同的方法可以解决这个问题。
  * 要求使用空间复杂度为 O(1) 的 原地 算法。
- * 
- * 
+ *
+ *
  */
 
 // @lc code=start
@@ -49,7 +49,20 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function(nums, k) {
-    
+  k = k % nums.length;
+  let n = nums.length - k;
+  reverse(nums, 0, n - 1);
+  reverse(nums, n, nums.length - 1);
+  reverse(nums, 0, nums.length - 1);
 };
-// @lc code=end
 
+function reverse(nums, start, end) {
+  let i = start;
+  let j = end;
+  while (i < j) {
+    [nums[i], nums[j]] = [nums[j], nums[i]];
+    i += 1;
+    j -= 1;
+  }
+}
+// @lc code=end
