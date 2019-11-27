1908e102c04c1a759b1849318755bd9ff72cb64b
huangjian <1342042894@qq.com>
2019-11-27 08:06:13 +0800

    add n queens

--- javascript-leetcode/51.n皇后.js
+++ javascript-leetcode/51.n皇后.js
@@ -14,32 +14,32 @@
  * Testcase Example:  '4'
  *
  * n 皇后问题研究的是如何将 n 个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。
- * 
- * 
- * 
+ *
+ *
+ *
  * 上图为 8 皇后问题的一种解法。
- * 
+ *
  * 给定一个整数 n，返回所有不同的 n 皇后问题的解决方案。
- * 
+ *
  * 每一种解法包含一个明确的 n 皇后问题的棋子放置方案，该方案中 'Q' 和 '.' 分别代表了皇后和空位。
- * 
+ *
  * 示例:
- * 
+ *
  * 输入: 4
  * 输出: [
  * ⁠[".Q..",  // 解法 1
  * ⁠ "...Q",
  * ⁠ "Q...",
  * ⁠ "..Q."],
- * 
+ *
  * ⁠["..Q.",  // 解法 2
  * ⁠ "Q...",
  * ⁠ "...Q",
  * ⁠ ".Q.."]
  * ]
  * 解释: 4 皇后问题存在两个不同的解法。
- * 
- * 
+ *
+ *
  */
 
 // @lc code=start
@@ -48,7 +48,56 @@
  * @return {string[][]}
  */
 var solveNQueens = function(n) {
-    
-};
-// @lc code=end
+  const ctx = {};
+  ctx.n = n;
+  ctx.cols = new Set();
+  ctx.pie = new Set();
+  ctx.na = new Set();
+  ctx.row = 0;
+  ctx.curResult = [];
+  ctx.result = [];
+  dfs(ctx);
 
+  for (let i = 0; i < ctx.result.length; i += 1) {
+    for (let j = 0; j < ctx.result[i].length; j += 1) {
+      const x = ctx.result[i][j];
+      ctx.result[i][j] = '.'.repeat(x) + 'Q' + '.'.repeat(n - x - 1);
+    }
+  }
+  return ctx.result;
+};
+
+function dfs(ctx) {
+  if (ctx.row >= ctx.n) {
+    ctx.result.push(ctx.curResult.slice(0));
+    return;
+  }
+
+  for (let col = 0; col < ctx.n; col += 1) {
+    if (
+      ctx.cols.has(col) ||
+      ctx.pie.has(ctx.row + col) ||
+      ctx.na.has(ctx.row - col)
+    ) {
+      continue;
+    }
+
+    ctx.cols.add(col);
+    ctx.pie.add(ctx.row + col);
+    ctx.na.add(ctx.row - col);
+    ctx.row += 1;
+    ctx.curResult.push(col);
+
+    dfs(ctx);
+
+    ctx.curResult.pop();
+    ctx.row -= 1;
+    ctx.cols.delete(col);
+    ctx.pie.delete(ctx.row + col);
+    ctx.na.delete(ctx.row - col);
+  }
+}
+
+const result = solveNQueens(4);
+console.log(result);
+// @lc code=end
--- javascript-leetcode/52.n皇后-ii.js
+++ javascript-leetcode/52.n皇后-ii.js
@@ -14,15 +14,15 @@
  * Testcase Example:  '4'
  *
  * n 皇后问题研究的是如何将 n 个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。
- * 
- * 
- * 
+ *
+ *
+ *
  * 上图为 8 皇后问题的一种解法。
- * 
+ *
  * 给定一个整数 n，返回 n 皇后不同的解决方案的数量。
- * 
+ *
  * 示例:
- * 
+ *
  * 输入: 4
  * 输出: 2
  * 解释: 4 皇后问题存在如下两个不同的解法。
@@ -31,14 +31,14 @@
  * "...Q",
  * "Q...",
  * "..Q."],
- * 
+ *
  * ["..Q.",  // 解法 2
  * "Q...",
  * "...Q",
  * ".Q.."]
  * ]
- * 
- * 
+ *
+ *
  */
 
 // @lc code=start
@@ -47,7 +47,56 @@
  * @return {number}
  */
 var totalNQueens = function(n) {
-    
-};
-// @lc code=end
+  const ctx = {};
+  ctx.n = n;
+  ctx.cols = new Set();
+  ctx.pie = new Set();
+  ctx.na = new Set();
+  ctx.row = 0;
+  ctx.curResult = [];
+  ctx.result = [];
+  dfs(ctx);
 
+  for (let i = 0; i < ctx.result.length; i += 1) {
+    for (let j = 0; j < ctx.result[i].length; j += 1) {
+      const x = ctx.result[i][j];
+      ctx.result[i][j] = '.'.repeat(x) + 'Q' + '.'.repeat(n - x - 1);
+    }
+  }
+  return ctx.result.length;
+};
+
+function dfs(ctx) {
+  if (ctx.row >= ctx.n) {
+    ctx.result.push(ctx.curResult.slice(0));
+    return;
+  }
+
+  for (let col = 0; col < ctx.n; col += 1) {
+    if (
+      ctx.cols.has(col) ||
+      ctx.pie.has(ctx.row + col) ||
+      ctx.na.has(ctx.row - col)
+    ) {
+      continue;
+    }
+
+    ctx.cols.add(col);
+    ctx.pie.add(ctx.row + col);
+    ctx.na.add(ctx.row - col);
+    ctx.row += 1;
+    ctx.curResult.push(col);
+
+    dfs(ctx);
+
+    ctx.curResult.pop();
+    ctx.row -= 1;
+    ctx.cols.delete(col);
+    ctx.pie.delete(ctx.row + col);
+    ctx.na.delete(ctx.row - col);
+  }
+}
+
+const result = totalNQueens(4);
+console.log(result);
+// @lc code=end
