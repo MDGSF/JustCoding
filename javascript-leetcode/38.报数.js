1ebec2b56d305a7d7155f7a1026add026aa6a312
huangjian <1342042894@qq.com>
2019-12-05 23:37:15 +0800

    add some code

--- javascript-leetcode/38.报数.js
+++ javascript-leetcode/38.报数.js
@@ -14,36 +14,36 @@
  * Testcase Example:  '1'
  *
  * 报数序列是一个整数序列，按照其中的整数的顺序进行报数，得到下一个数。其前五项如下：
- * 
+ *
  * 1.     1
  * 2.     11
  * 3.     21
  * 4.     1211
  * 5.     111221
- * 
- * 
+ *
+ *
  * 1 被读作  "one 1"  ("一个一") , 即 11。
  * 11 被读作 "two 1s" ("两个一"）, 即 21。
  * 21 被读作 "one 2",  "one 1" （"一个二" ,  "一个一") , 即 1211。
- * 
+ *
  * 给定一个正整数 n（1 ≤ n ≤ 30），输出报数序列的第 n 项。
- * 
+ *
  * 注意：整数顺序将表示为一个字符串。
- * 
- * 
- * 
+ *
+ *
+ *
  * 示例 1:
- * 
+ *
  * 输入: 1
  * 输出: "1"
- * 
- * 
+ *
+ *
  * 示例 2:
- * 
+ *
  * 输入: 4
  * 输出: "1211"
- * 
- * 
+ *
+ *
  */
 
 // @lc code=start
@@ -52,7 +52,43 @@
  * @return {string}
  */
 var countAndSay = function(n) {
-    
+  if (n === 1) {
+    return '1';
+  }
+  let cur = '1';
+  for (let i = 2; i <= n; i += 1) {
+    cur = next(cur);
+  }
+  return cur;
 };
-// @lc code=end
 
+function next(num) {
+  let result = '';
+  let pre = null;
+  let count = 0;
+  for (let i = 0; i < num.length; i += 1) {
+    if (pre === null) {
+      pre = num.charAt(i);
+      count += 1;
+      continue;
+    }
+
+    if (pre === num.charAt(i)) {
+      count += 1;
+      continue;
+    }
+
+    result = result + count + pre;
+    pre = num.charAt(i);
+    count = 1;
+  }
+  if (count > 0) {
+    result = result + count + pre;
+  }
+  return result;
+}
+
+const n = 5;
+const result = countAndSay(n);
+console.log(result);
+// @lc code=end
