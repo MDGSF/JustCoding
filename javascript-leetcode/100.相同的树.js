17396e34583ab28c714a2d5be989ac0f7023cd3e
HuangJian <1342042894@qq.com>
2020-01-01 23:41:50 +0800

    modify algorithmNote, add some code

--- javascript-leetcode/100.相同的树.js
+++ javascript-leetcode/100.相同的树.js
@@ -14,41 +14,41 @@
  * Testcase Example:  '[1,2,3]\n[1,2,3]'
  *
  * 给定两个二叉树，编写一个函数来检验它们是否相同。
- * 
+ *
  * 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
- * 
+ *
  * 示例 1:
- * 
+ *
  * 输入:       1         1
  * ⁠         / \       / \
  * ⁠        2   3     2   3
- * 
+ *
  * ⁠       [1,2,3],   [1,2,3]
- * 
+ *
  * 输出: true
- * 
+ *
  * 示例 2:
- * 
+ *
  * 输入:      1          1
  * ⁠         /           \
  * ⁠        2             2
- * 
+ *
  * ⁠       [1,2],     [1,null,2]
- * 
+ *
  * 输出: false
- * 
- * 
+ *
+ *
  * 示例 3:
- * 
+ *
  * 输入:       1         1
  * ⁠         / \       / \
  * ⁠        2   1     1   2
- * 
+ *
  * ⁠       [1,2,1],   [1,1,2]
- * 
+ *
  * 输出: false
- * 
- * 
+ *
+ *
  */
 
 // @lc code=start
@@ -65,7 +65,10 @@
  * @return {boolean}
  */
 var isSameTree = function(p, q) {
-    
+  if (p === null && q === null) { return true; }
+  if (p === null || q === null) { return false; }
+  if (p.val !== q.val) { return false; }
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 };
 // @lc code=end
 
