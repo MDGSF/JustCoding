029ed50c0c10ea639b348686bcabc03de1a88673
HuangJian <huangjian@minieye.cc>
2022-12-08 08:33:06 +0800

    update

--- rust-leetcode/leetcode_72/src/main.rs
+++ rust-leetcode/leetcode_72/src/main.rs
@@ -1,37 +1,57 @@
+#![cfg_attr(test, feature(test))]
+
+#[cfg(test)]
+extern crate test;
+
 impl Solution {
-  pub fn min_distance(word1: String, word2: String) -> i32 {
-    use std::cmp::min;
-    let chars1: Vec<char> = word1.chars().collect();
-    let chars2: Vec<char> = word2.chars().collect();
-    let rows = word1.len() + 1;
-    let cols = word2.len() + 1;
-    let mut dp = vec![vec![0; cols]; rows];
-    for row in 1..rows {
-      dp[row][0] = row;
-    }
-    for col in 1..cols {
-      dp[0][col] = col;
-    }
-    for row in 1..rows {
-      for col in 1..cols {
-        if chars1[row - 1] == chars2[col - 1] {
-          dp[row][col] = dp[row - 1][col - 1];
-        } else {
-          dp[row][col] = 1
-            + min(
-              min(dp[row - 1][col - 1], dp[row - 1][col]),
-              dp[row][col - 1],
-            );
+    pub fn min_distance(word1: String, word2: String) -> i32 {
+        use std::cmp::min;
+        let chars1: Vec<char> = word1.chars().collect();
+        let chars2: Vec<char> = word2.chars().collect();
+        let rows = word1.len() + 1;
+        let cols = word2.len() + 1;
+        let mut dp = vec![vec![0; cols]; rows];
+        for row in 1..rows {
+            dp[row][0] = row;
         }
-      }
+        for col in 1..cols {
+            dp[0][col] = col;
+        }
+        for row in 1..rows {
+            for col in 1..cols {
+                if chars1[row - 1] == chars2[col - 1] {
+                    dp[row][col] = dp[row - 1][col - 1];
+                } else {
+                    dp[row][col] = 1 + min(
+                        min(dp[row - 1][col - 1], dp[row - 1][col]),
+                        dp[row][col - 1],
+                    );
+                }
+            }
+        }
+        dp[rows - 1][cols - 1] as i32
     }
-    dp[rows - 1][cols - 1] as i32
-  }
 }
 
 struct Solution;
 
 fn main() {
-  let result = Solution::min_distance("horse".to_string(), "ros".to_string());
-  println!("result = {}", result);
+    let result = Solution::min_distance("horse".to_string(), "ros".to_string());
+    println!("result = {}", result);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test::Bencher;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[bench]
+    fn benchmark(bencher: &mut Bencher) {
+        bencher.iter(|| Solution::min_distance("horse".to_string(), "ros".to_string()));
+    }
 }
